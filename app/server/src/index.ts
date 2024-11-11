997797e7ecc290f4110363055c9e85f176ac20a4
Aliph0th <theblacklord05@gmail.com>
2024-11-11 21:10:11 +0300

    feat(server): init index file & install additional packages

---
+++ app/server/src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = +(process.env.PORT || 3000);
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
