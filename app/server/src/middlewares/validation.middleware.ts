2c8941d7f09a62770d1ac1d533e61792a7800dcd
Aliph0th <theblacklord05@gmail.com>
2024-11-13 00:42:12 +0300

    feat(server): add validation middleware and decomposed validators

---
+++ app/server/src/middlewares/validation.middleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+import { BadRequestException } from '../exceptions';
+
+export const validationResultMiddleware = (req: Request, res: Response, next: NextFunction) => {
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      next(new BadRequestException(errors.array()));
+      return;
+   }
+   next();
+};
